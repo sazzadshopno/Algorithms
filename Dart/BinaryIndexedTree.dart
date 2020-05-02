b7edbd227844633c8c5c6fa151973cb070ef2d7d
Sazzad Shopno <Sazzadshopno6@gmail.com>
2020-05-02 20:45:41 +0600

    BIT in dart

---
+++ Dart/BinaryIndexedTree.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+const num mx = 6;
+List<int> arr = List.filled(mx, 0), tree = List.filled(mx, 0);
+void update(int idx, int val, int n) {
+  while (idx <= n) {
+    tree[idx] += val;
+    idx += (idx & -idx);
+  }
+}
+
+int query(int idx) {
+  int sum = 0;
+  while (idx > 0) {
+    sum += tree[idx];
+    idx -= (idx & -idx);
+  }
+  return sum;
+}
+
+int main() {
+  num n = 5;
+  for (int i = 1; i <= n; i++) {
+    arr[i] = Random().nextInt(150);
+    update(i, arr[i], n);
+  }
+  print(arr);
+  int p = 5, q = 3;
+  print(query(p) - query(q - 1));
+  return 0;
+}
